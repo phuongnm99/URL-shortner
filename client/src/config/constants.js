1fa1854e2bae9b8fe24416733a66b9ed3c64753a
Mai Hương <huongnm.aime@gmail.com>
2020-04-06 09:07:06 +0700

    commit

---
+++ client/src/config/constants.js
@@ -0,0 +1,7 @@
+module.exports = {
+  apiUrl: "http://localhost:7000/api/",
+  baseUrl: "http://localhost" 
+  // baseUrl: "http://aimesoft.com"
+  //  apiUrl: "http://shortener.muhzi.com/v1/api/",
+  // baseUrl: "http://muhzi.com" 
+};
